fea174387b2902a82f8889d3142d688e80c62771
dany gbelidji <dany.gbelidji@live.fr>
2021-03-22 22:18:15 +0100

    DOC : Ajout d'un fichier to_do.txt

--- src/filesList.js
+++ src/filesList.js
@@ -2,10 +2,8 @@ export const filesList = [
     { nom: "Main.js", type: "code" },
     { nom: "Music.mp3", type: "music" },
     { nom: "Vacances.jpg", type: "image" },
-    { nom: "Contract.pdf", type: "pdf" },
-    { nom: "Guitar.mp3", type: "music" },
-    { nom: "music.mp3", type: "music" },
-    { nom: "photo.jpg", type: "image" }
+    { nom: "Contract.pdf", type: "pdf" }
 ]
+
 
 export const typeFile = ["code", "music", "image", "pdf"];
